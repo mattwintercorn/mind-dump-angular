40109f70926acc11d58698908e5a3139fa28c547
Matt Wintercorn <matthew.wintercorn@cesicorp.com>
2026-04-30 20:53:39 -0400

    feat: add ideas route with IdeaListComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { IdeaListComponent } from './features/ideas/components/idea-list/idea-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/ideas', pathMatch: 'full' },
+  { path: 'ideas', component: IdeaListComponent }
+];
