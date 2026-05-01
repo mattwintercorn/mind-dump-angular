0eb7d58662164edf83b22d9d9cec064c1c6833e7
Matt Wintercorn <matthew.wintercorn@cesicorp.com>
2026-04-30 21:45:23 -0400

    Add service worker update notifications
    - Implement SwUpdate to check for new versions
    - Add MatSnackBar notifications for available updates
    - Auto-check for updates every hour
    - Allow users to reload to get latest version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SwUpdate } from '@angular/service-worker';
 import { ToolbarComponent, ViewMode } from './shared/components/layout/toolbar/toolbar.component';
 import { IdeaFormComponent } from './features/ideas/components/idea-form/idea-form.component';
 import { IdeaService } from './core/services/idea.service';
@@ -12,12 +14,38 @@ import { IdeaService } from './core/services/idea.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   private dialog = inject(MatDialog);
   private ideaService = inject(IdeaService);
   private router = inject(Router);
+  private swUpdate = inject(SwUpdate);
+  private snackBar = inject(MatSnackBar);
   
   viewMode: ViewMode = 'grid';
+
+  ngOnInit(): void {
+    if (this.swUpdate.isEnabled) {
+      // Check for updates every hour
+      setInterval(() => {
+        this.swUpdate.checkForUpdate();
+      }, 60 * 60 * 1000);
+
+      // Listen for available updates
+      this.swUpdate.versionUpdates.subscribe(event => {
+        if (event.type === 'VERSION_READY') {
+          const snackBarRef = this.snackBar.open(
+            'New version available! Reload to update.',
+            'Reload',
+            { duration: 0 }
+          );
+
+          snackBarRef.onAction().subscribe(() => {
+            window.location.reload();
+          });
+        }
+      });
+    }
+  }
 
   onNewIdea(): void {
     const dialogRef = this.dialog.open(IdeaFormComponent, {
